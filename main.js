c12ce248e1345c4ea76d6599f39f63bb98aae58d
Zuzu work <muratov01@mail.ru>
2023-04-27 23:50:07 +0600

    домашка 7

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+
+                                //домашка 7.1
+
+
+// let btn=document.querySelector('.checker') 
+// let out= document.querySelector('.out')
+// let a = 20
+// function play(){
+//     out.innerHTML= a
+// }
+// btn.onclick= play
+
+
+                                      //7.2
+
+let btn = document.querySelector('.checker')
+let a =36
+let b = 12
+
+function multiply(){
+console.log(a*b);
+   console.log(a/b);
+   console.log(a+b);
+   console.log(a-b);
+}
+multiply()
+
+
+// второе задание хотела через Клик, но забыла команду 
